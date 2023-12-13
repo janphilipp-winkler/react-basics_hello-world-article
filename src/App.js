488e68b3304cab255113b231aff225eaa21e80b8
Jan-Philipp Winkler <janphilipp.winkler@gmail.com>
2023-12-13 11:17:43 +0100

    aufgabe gemacht

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React from "react";
 import "./styles.css";
 
+function HelloWorldArticle() {
+  return (
+    <>
+      <article>
+        <h1>Hello World!</h1>
+        <p>This is a paragraph.</p>
+      </article>
+    </>
+  );
+}
+
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return <HelloWorldArticle />;
 }
